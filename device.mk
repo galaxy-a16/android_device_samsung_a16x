aea9fca742dd677213c4548083d6cf70debbdf36
Jamie Falconer <mrfluffyoven@gmail.com>
2024-11-19 19:34:47 +0000

    add fastbootd

--- device.mk
+++ device.mk
@@ -6,3 +6,34 @@
 #
 
 LOCAL_PATH := device/samsung/a16x
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd 
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 34
